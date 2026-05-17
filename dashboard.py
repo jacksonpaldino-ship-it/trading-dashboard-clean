a0cc4c885f686d253ec45f7c7f12ed4e168427c8
jacksonpaldino-ship-it <jackson.paldino@gmail.com>
2026-05-17 13:04:48 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
-st.title("Trading Dashboard")
+st.title("📈 Trading Dashboard")
 
-st.success("Deployment works")
+st.success("Dashboard Online")
+
+# =====================================
+# FAKE TEST DATA
+# =====================================
+
+data = pd.DataFrame({
+    "Equity": [2000, 2020, 2010, 2055]
+})
+
+st.line_chart(data)
